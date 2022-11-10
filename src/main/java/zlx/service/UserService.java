f3261d848d5b119f100c6de2ec856966fe56ee95
zhaolixin <2536338590@qq.com>
2022-11-10 22:46:03 +0800

    增加内容

--- src/main/java/zlx/service/UserService.java
+++ src/main/java/zlx/service/UserService.java
@@ -1,6 +1,8 @@
 package zlx.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import zlx.dao.UserDao;
@@ -17,12 +19,9 @@ public class UserService {
     }
 
 
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
+
+
     @EventListener(classes = ApplicationEvent.class)
     public void listen(ApplicationEvent event){
         System.out.println("UserServer...监听到的事件"+event);
